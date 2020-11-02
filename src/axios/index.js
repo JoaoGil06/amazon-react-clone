635105b7a8b5520c5b7a803f0e58f72b4c5003ba
João Gil <joaogmonteiro06@gmail.com>
2020-11-02 19:48:07 +0000

    Update with assets

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-169b5/us-central1/api",
+  //baseURL: "http://localhost:5001/clone-169b5/us-central1/api",
+  baseURL: "https://us-central1-clone-169b5.cloudfunctions.net/api",
 });
 
 export default instance;
